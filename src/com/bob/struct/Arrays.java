6313a5c60fd0bfabeefc179960ebaa2cab9f218a
waveletbob <410015031@qq.com>
2020-03-21 11:12:57 +0800

    1
    1

---
+++ src/com/bob/struct/Arrays.java
@@ -0,0 +1,10 @@
+package com.bob.struct;
+
+public class Arrays {
+    int[] array={};
+    int [] array1=new int[3];
+    int[] array2=new int[]{};
+    public static void main(String[] args) {
+
+    }
+}
